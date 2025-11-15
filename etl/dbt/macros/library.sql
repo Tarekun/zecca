8fa353743445ab191faedbfb1b6de1b23fce2a4a
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2025-11-15 19:23:42 +0100

    refactor

--- etl/dbt/macros/library.sql
+++
@@ -1,7 +0,0 @@
-{% macro safe_return(entry_value, current_value) %}
-    CASE 
-        WHEN {{entry_value}} > 0 AND {{current_value}} > 0
-            THEN LN({{entry_value}} / {{current_value}})
-        ELSE NULL
-    END
-{% endmacro %}
