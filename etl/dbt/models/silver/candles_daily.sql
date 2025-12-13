83be3baca25c77f4287ecff277fb96951672141b
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2025-12-13 20:00:01 +0100

    revert config materialized config

--- etl/dbt/models/silver/candles_daily.sql
+++ etl/dbt/models/silver/candles_daily.sql
@@ -1,8 +1,6 @@
 {{
     config(
-        materialized="incremental",
-        format="parquet",
-        post_hook="{{ materialized_partitioned_parquet(['year','month']) }}",
+        materialized="table",
     )
 }}
 
