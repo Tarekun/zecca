b9d92c3f5a6d46a676eef7100167c4bd1ec0f963
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2026-06-15 22:01:48 +0200

    exports models from silver model

---
+++ etl/transformation/silver/__init__.py
@@ -0,0 +1,13 @@
+from etl.transformation.silver.candles_daily import CandlesDailySilver
+from etl.transformation.silver.company_tickers import CompanyTickersSilver
+from etl.transformation.silver.sec_company_facts import SecCompanyFactsSilver
+from etl.transformation.silver.sec_company_facts_padded import SecCompanyFactsPaddedSilver
+from etl.transformation.silver.stocks_daily import StocksDailySilver
+
+__all__ = [
+    "CandlesDailySilver",
+    "CompanyTickersSilver",
+    "SecCompanyFactsSilver",
+    "SecCompanyFactsPaddedSilver",
+    "StocksDailySilver",
+]
