cc907b47fb7f95adf988eca86d19c2543f6bf895
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2026-06-26 23:29:04 +0200

    implemented lazy loading functionality of models

--- etl/transformation/silver/stocks_daily.py
+++ etl/transformation/silver/stocks_daily.py
@@ -1,66 +1,13 @@
-import duckdb
 import polars as pl
-from pathlib import Path
 
 from etl.logger import get_logger
-from etl.transformation.model import Model, DATAPLATFORM_ROOT
+from etl.transformation.model import Model
 from etl.transformation.silver.candles_daily import CandlesDailySilver
-from etl.transformation.silver.company_tickers import CompanyTickersSilver
 from etl.transformation.silver.sec_company_facts_padded import (
     SecCompanyFactsPaddedSilver,
 )
 
 logger = get_logger(__name__)
-
-
-def compute_with_polars() -> pl.DataFrame:
-    logger.debug("Using source: CandlesDailySilver, SecCompanyFactsPaddedSilver")
-    candles = CandlesDailySilver("").load_from_disk()
-    sec = (
-        SecCompanyFactsPaddedSilver()
-        .load_from_disk()
-        .select(
-            ["ticker", "reference_date", "shares_outstanding", "estimated_float_shares"]
-        )
-    )
-
-    df = candles.join(
-        sec,
-        left_on=["symbol", "timeframe"],
-        right_on=["ticker", "reference_date"],
-        how="left",
-    ).with_columns((pl.col("shares_outstanding") * pl.col("open")).alias("evaluation"))
-
-    return df
-
-
-# TODO handle this better
-_CANDLES_GLOB = str(
-    Path(DATAPLATFORM_ROOT) / "silver" / "candles_daily" / "**" / "*.parquet"
-)
-_SEC_PATH = str(
-    Path(DATAPLATFORM_ROOT)
-    / "silver"
-    / "sec_company_facts_padded"
-    / "sec_company_facts_padded.parquet"
-)
-
-
-def compute_with_duckdb() -> pl.DataFrame:
-    logger.debug("Using source: CandlesDailySilver, SecCompanyFactsPaddedSilver")
-    with duckdb.connect() as conn:
-        return conn.execute(f"""
-            SELECT
-                c.*,
-                s.shares_outstanding,
-                s.estimated_float_shares,
-                s.shares_outstanding * c.open AS evaluation
-            FROM read_parquet('{_CANDLES_GLOB}', hive_partitioning = true) c
-            LEFT JOIN (
-                SELECT ticker, reference_date, shares_outstanding, estimated_float_shares
-                FROM read_parquet('{_SEC_PATH}')
-            ) s ON c.symbol = s.ticker AND c.timeframe = s.reference_date
-        """).pl()
 
 
 class StocksDailySilver(Model):
@@ -72,5 +19,16 @@ class StocksDailySilver(Model):
         )
         self.configure_dependencies([CandlesDailySilver, SecCompanyFactsPaddedSilver])
 
-    def _build(self) -> pl.DataFrame:
-        return compute_with_duckdb()
+    def _build(self) -> pl.LazyFrame:
+        candles = CandlesDailySilver("").lazy_load()
+        sec = SecCompanyFactsPaddedSilver().lazy_load().select(
+            ["ticker", "reference_date", "shares_outstanding", "estimated_float_shares"]
+        )
+        return candles.join(
+            sec,
+            left_on=["symbol", "timeframe"],
+            right_on=["ticker", "reference_date"],
+            how="left",
+        ).with_columns(
+            (pl.col("shares_outstanding") * pl.col("open")).alias("evaluation")
+        )
