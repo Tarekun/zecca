1bf252624e3aed9caa710f54bf7414e554ba1c9f
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2026-07-18 00:19:48 +0200

    moved data quality tests to lazyframe

--- tests/data_quality/silver/test_company_tickers.py
+++ tests/data_quality/silver/test_company_tickers.py
@@ -8,14 +8,13 @@ sys.path.append(str(Path(__file__).parents[3]))
 
 from etl.transformation.silver.company_tickers import CompanyTickersSilver
 
-_TEST_OUTPUTS = Path(__file__).parents[3] / "dataplatform" / "test_outputs"
-
-_df = CompanyTickersSilver().read_from_disk().collect()
+lf = CompanyTickersSilver().read_from_disk()
 
 
 def test_no_null_cik_str():
     """No row in company_tickers should have a null cik_str."""
-    null_rows = _df.filter(pl.col("cik_str").is_null())
+
+    null_rows = lf.select("cik_str").filter(pl.col("cik_str").is_null()).collect()
 
     assert null_rows.height == 0, (
         f"Found {null_rows.height} row(s) with a null cik_str.\n"
@@ -25,7 +24,8 @@ def test_no_null_cik_str():
 
 def test_no_null_ticker():
     """No row in company_tickers should have a null ticker."""
-    null_rows = _df.filter(pl.col("ticker").is_null())
+
+    null_rows = lf.select("ticker").filter(pl.col("ticker").is_null()).collect()
 
     assert null_rows.height == 0, (
         f"Found {null_rows.height} row(s) with a null ticker.\n"
